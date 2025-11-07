2875c271b33e33c571dda7f99cbb49c1adcf4876
ALEX ROMULO PEREIRA ALMEIDA <romulopereiraalmeida@gmail.com>
2025-11-06 23:52:01 -0300

    fix(nav): scroll suave para seções sem quebrar HashRouter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
 export default function Navbar() {
+  const scrollTo = (id) => (e) => {
+    e.preventDefault()
+    const el = document.getElementById(id)
+    if (el) el.scrollIntoView({ behavior: 'smooth' })
+  }
   return (
     <header className="navbar">
       <div className="container navbar__inner">
-        <a className="brand" href="#home">Tech Curiosidades</a>
+        <a className="brand" href="#" onClick={scrollTo('home')}>Tech Curiosidades</a>
         <nav className="nav">
-          <a className="nav__link" href="#home">Início</a>
-          <a className="nav__link" href="#about">Sobre</a>
-          <a className="nav__link" href="#posts">Posts</a>
-          <a className="nav__link" href="#products">Produtos</a>
-          <a className="nav__link" href="#contact">Contato</a>
+          <a className="nav__link" href="#" onClick={scrollTo('home')}>Início</a>
+          <a className="nav__link" href="#" onClick={scrollTo('about')}>Sobre</a>
+          <a className="nav__link" href="#" onClick={scrollTo('posts')}>Posts</a>
+          <a className="nav__link" href="#" onClick={scrollTo('products')}>Produtos</a>
+          <a className="nav__link" href="#" onClick={scrollTo('contact')}>Contato</a>
         </nav>
       </div>
     </header>
